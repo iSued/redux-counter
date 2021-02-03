205f591d4ecc3bbfbebb225baeb60bffb23ff2c9
iSued <59610863+iSued@users.noreply.github.com>
2021-02-03 09:58:44 +0100

    update

--- src/index.js
+++ src/index.js
@@ -1,10 +1,36 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
-
 import './styles.scss';
+import redux from 'redux';
+
+import { createStore } from 'redux';
+import { connect, Provider } from 'react-redux';
+
+const initialState = {
+  count: 0,
+};
+
+const incrementValue = () => ({
+  type: INCREMENT,
+});
+
+const INCREMENT = 'INCREMENT';
+const reducer = (state = initialState, action) => {
+  if (action.type === INCREMENT) {
+    return {
+      count: state.count + 1,
+    };
+  }
+
+  return state;
+};
+
+const store = createStore(reducer);
 
 class Counter extends Component {
   render() {
+    const { count, increment } = this.props;
+    console.log({ count, increment });
     return (
       <main className="Counter">
         <p className="count">0</p>
@@ -17,5 +43,24 @@ class Counter extends Component {
     );
   }
 }
+//Allow to take what we want froms store
+const mapStateToProps = (state) => {
+  return state;
+};
+//
+const mapDispatchToProps = (dispatch) => {
+  return {
+    increment() {
+      dispatch(incrementValue());
+    },
+  };
+};
 
-render(<Counter />, document.getElementById('root'));
+connect()(Counter);
+
+render(
+  <Provider store={store}>
+    <Counter />
+  </Provider>,
+  document.getElementById('root'),
+);
